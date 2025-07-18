a3237305fd7c697dfcd20698c8c1b476f88198cc
Angel-enc <enock8876@gmail.com>
2025-07-18 01:05:12 +0000

    solucionado el error NaN

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,33 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import 'bootstrap';
+import PropTypes from 'prop-types';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+function SecondsCounter(props) {
+	const digitFour = Number(props.digitFour) || 0;
+	const digitThree = Number(props.digitThree) || 0;
+	const digitTwo = Number(props.digitTwo) || 0;
+	const digitOne = Number(props.digitOne) || 0;
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	return (<div className="master d-flex justify-content-center align-items-center fs-1">
+		<div className="reloj p-2 bg-dark text-white rounded">
+			<i className="fas fa-clock"></i>
 		</div>
-	);
+		<div className="four p-2">{digitFour % 10}</div>
+		<div className="three p-2">{digitThree % 10}</div>
+		<div className="two p-2">{digitTwo % 10}</div>
+		<div className="one p-2">{digitOne % 10}</div>
+	</div>);
+}
+SecondsCounter.propTypes = {
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
 };
 
-export default Home;
+
+
+export default SecondsCounter
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { createRoot } from 'react-dom/client';
+import SecondsCounter from './components/Home';
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
@@ -10,9 +11,23 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+
+let counter = 0
+
+setInterval(() => {
+  const four = Math.floor(counter / 1000) % 10;
+  const three = Math.floor(counter / 100) % 10;
+  const two = Math.floor(counter / 10) % 10;
+  const one = counter % 10;
+
+  counter++;
+
+  root.render(
+    <SecondsCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />
+
+  );
+}, 1000);
+
